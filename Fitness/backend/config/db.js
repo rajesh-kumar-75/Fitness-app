601b819e173c9fbdff39c3a9c933c5819db625aa
Your Name <rajeshadugula501@gmail.com>
2026-09-21 09:35:13 +0530

    newadding

--- Fitness/backend/config/db.js
+++ Fitness/backend/config/db.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
  */
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const uri = process.env.MONGO_URI || process.env.MONGODB_URI;
+    const conn = await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000,
     });
     console.log(`MongoDB connected successfully: ${conn.connection.host}`);
